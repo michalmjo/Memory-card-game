a62740be2c16913aeac805adbc7c8fd98ad04326
Michał Ożóg <m.ozog@protonmail.com>
2020-12-22 12:26:47 +0100

    fix point event when cards is pair

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const cardsImg = ["img/1.jpg", "img/1.jpg", "img/2.jpg", "img/2.jpg", "img/3.jpg", "img/3.jpg", "img/4.jpg", "img/4.jpg", "img/5.jpg", "img/5.jpg", "img/6.jpg", "img/6.jpg", "img/7.jpg", "img/7.jpg", "img/8.jpg", "img/8.jpg"];
-
-
+const cardsImg = [
+  "img/1.jpg",
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/8.jpg",
+];
 
 let cards = document.querySelectorAll("div.container div img");
 
 cards = [...cards];
 const startTime = new Date().getTime();
-
 
 let activeCard = "";
 const activeCards = [];
@@ -14,75 +28,73 @@ const activeCards = [];
 const gamePairs = cards.length / 2;
 let gameResoult = 0;
 
-
-
 cards.ondragstart = function () {
-    return false;
+  return false;
 };
 const clickCard = function () {
-    activeCard = this;
-    if (activeCard == activeCards[0]) return;
-    activeCard.classList.remove("hidden");
+  activeCard = this;
+  if (activeCard == activeCards[0]) return;
+  activeCard.classList.remove("hidden");
 
-    if (activeCards.length === 0) {
-        activeCards[0] = activeCard;
-        return;
-    }
+  if (activeCards.length === 0) {
+    activeCards[0] = activeCard;
+    return;
+  }
 
-    // 2 klik
-    else {
-        cards.forEach(card => {
-            card.removeEventListener("click", clickCard);
-        })
-        activeCards[1] = activeCard;
-
-        setTimeout(function () {
-            if (activeCards[0].getAttribute('src') === activeCards[1].getAttribute('src')) {
-                console.log("win");
-                activeCards.forEach(card => {
-                    card.setAttribute('src', "img/done.jpg")
-                })
-                gameResoult++;
-
-                if (gameResoult == gamePairs) {
-                    console.log("win game");
-                    const endTime = new Date().getTime();
-                    const gameTime = (endTime - startTime) / 1000;
-                    setTimeout(function () {
-                        alert(`Wygrałeś twój wynik to: ${gameTime}`);
-                        location.reload();
-                    }, 200);
-
-                }
-            } else {
-                activeCards.forEach(card => card.classList.add("hidden"))
-            }
-            activeCard = "";
-            activeCards.length = 0;
-            cards.forEach(card => {
-                card.addEventListener("click", clickCard);
-            })
-        }, 700)
-
-    }
-
-
-}
-
-const init = () => {
-    cards.forEach(card => {
-        const position = Math.floor(Math.random() * cardsImg.length);
-        card.setAttribute('src', cardsImg[position]);
-        cardsImg.splice(position, 1);
-    })
+  // 2 klik
+  else {
+    cards.forEach((card) => {
+      card.removeEventListener("click", clickCard);
+    });
+    activeCards[1] = activeCard;
 
     setTimeout(function () {
-        cards.forEach((card) => {
-            card.classList.add("hidden");
-            card.addEventListener("click", clickCard);
-            card.setAttribute('draggable', false);
-        })
-    }, 2000)
-}
+      if (
+        activeCards[0].getAttribute("src") ===
+        activeCards[1].getAttribute("src")
+      ) {
+        console.log("winaaaa");
+        activeCards.forEach((card) => {
+          card.setAttribute("src", "img/done.jpg");
+          card.classList.add("noneActivity");
+        });
+        gameResoult++;
+
+        if (gameResoult == gamePairs) {
+          console.log("win game");
+          const endTime = new Date().getTime();
+          const gameTime = (endTime - startTime) / 1000;
+          setTimeout(function () {
+            alert(`Wygrałeś twój wynik to: ${gameTime}`);
+            location.reload();
+          }, 200);
+        }
+      } else {
+        activeCards.forEach((card) => card.classList.add("hidden"));
+      }
+      activeCard = "";
+      activeCards.length = 0;
+      cards.forEach((card) => {
+        card.addEventListener("click", clickCard);
+      });
+    }, 700);
+  }
+};
+
+const init = () => {
+  cards.forEach((card) => {
+    const position = Math.floor(Math.random() * cardsImg.length);
+    card.setAttribute("src", cardsImg[position]);
+    cardsImg.splice(position, 1);
+  });
+
+  setTimeout(function () {
+    cards.forEach((card) => {
+      card.classList.add("hidden");
+      card.addEventListener("click", clickCard);
+      card.setAttribute("draggable", false);
+    });
+  }, 2000);
+};
 
 init();
